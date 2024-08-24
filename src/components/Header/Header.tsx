28b5529075081ebe024382c9929a10eed3168653
Liomlong <529855617@qq.com>
2024-08-24 18:46:53 +0800

    更新 Header 组件以接收 title 属性，修复类型问题

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,18 @@
-import {TonConnectButton} from "@tonconnect/ui-react";
+import React from 'react';
+import { TonConnectButton } from "@tonconnect/ui-react";
 import './header.scss';
 
-export const Header = () => {
-    return <header>
-        <span>My App with React UI</span>
-        <TonConnectButton />
-    </header>
+interface HeaderProps {
+  title: string;
 }
+
+export const Header: React.FC<HeaderProps> = ({ title }) => {
+  return (
+    <header>
+      <span>{title}</span>
+      <TonConnectButton />
+    </header>
+  );
+}
+
+export default Header;
